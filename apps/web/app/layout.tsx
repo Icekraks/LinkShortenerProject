d6a256c6471d8045a41a19c622c04487ca41cb4b
Icekraks <felix.hu847@gmail.com>
2026-02-27 15:16:37 +1100

    feature: added vercel analytics/speed

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next"
 import { geistMono, lato } from "@/lib/fontHelper"
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css"
 import Header from "@/components/layout/Header"
@@ -19,6 +21,8 @@ export default function RootLayout({
       <body className={`${lato.variable} ${geistMono.variable} antialiased`}>
         <Header />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
