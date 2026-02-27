c42bc96d27429d828c603014065381a0585a2c89
Icekraks <felix.hu847@gmail.com>
2026-02-27 16:55:11 +1100

    feature: logo

--- apps/web/components/layout/Header.tsx
+++ apps/web/components/layout/Header.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link"
 import DesktopNavigation from "@components/layout/DesktopNavigation"
 import MobileNavigation from "@components/layout/MobileNavigation"
+import Image from "next/image"
 
 const Header = () => {
   return (
     <header className="flex justify-between items-center py-4 px-8 border-b">
       <span className="text-2xl font-bold">
-        <Link href="/">Snipr</Link>
+        <Link href="/" className="bg-zinc-400 p-4 rounded-md">
+          <Image
+            src="/sniprLogo.webp"
+            alt="Snipr Logo"
+            width={128}
+            height={80}
+            className="inline mr-2"
+          />
+        </Link>
       </span>
       <DesktopNavigation />
       <MobileNavigation />
