d20bec547a9c53ede9952a17048626adec6d68fb
Icekraks <felix.hu847@gmail.com>
2026-03-11 16:24:45 +1100

    feature: updated pr comments

--- apps/web/components/ui/sonner.tsx
+++ apps/web/components/ui/sonner.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useTheme } from "@hooks/useTheme"
+import type { CSSProperties } from "react"
 import { Toaster as Sonner, type ToasterProps } from "sonner"
 import {
   CircleCheckIcon,
@@ -31,7 +32,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
           "--normal-text": "var(--popover-foreground)",
           "--normal-border": "var(--border)",
           "--border-radius": "var(--radius)",
-        } as React.CSSProperties
+        } as CSSProperties
       }
       toastOptions={{
         classNames: {
