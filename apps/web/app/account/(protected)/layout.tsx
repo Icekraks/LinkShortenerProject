cf2d351169515ecef7922de567fc0dd52a47a783
Icekraks <felix.hu847@gmail.com>
2026-04-02 12:21:16 +1100

    feature: fixed dashboard

--- apps/web/app/account/(protected)/layout.tsx
+++ apps/web/app/account/(protected)/layout.tsx
@@ -4,7 +4,10 @@ import { getActiveSession } from "@/lib/authSession"
 import MobileDashboardNavigation from "@components/navigations/MobileDashboardNavigation"
 import DesktopDashboardNavigation from "@components/navigations/DesktopDashboardNavigation"
 
-const links: { name: string; href: string }[] = []
+const links: { name: string; href: string }[] = [
+  { name: "Dashboard", href: "/account/dashboard" },
+  { name: "Settings", href: "/account/settings" },
+]
 
 export default async function AccountProtectedLayout({ children }: { children: React.ReactNode }) {
   const session = await getActiveSession()
@@ -14,7 +17,7 @@ export default async function AccountProtectedLayout({ children }: { children: R
   }
 
   return (
-    <div className="flex flex-col md:flex-row min-h-screen justify-center font-sans px-4 gap-x-8 w-full py-8">
+    <div className="flex flex-col md:flex-row min-h-screen items-center justify-center font-sans px-4 gap-x-8 w-full py-8">
       <DesktopDashboardNavigation links={links} />
       <MobileDashboardNavigation links={links} />
       <main className="flex min-h-screen w-full md:w-3/4 flex-col items-center gap-y-4 md:gap-y-8 mt-8 md:mt-0">
